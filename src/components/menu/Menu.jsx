1dc541a16934f77a65f68046c39953fc5676fc81
tphuoc0104 <137038555+tphuoc0104@users.noreply.github.com>
2023-07-28 12:38:23 +0000

    starton menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -9,35 +9,45 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 const Menu = () => {
   return (
     <div className='menu'>
+
       <div className='logo'>
-        
         <img src='' alt="Logo"/>
       </div>
+
       <div className="navigate" >
         
-        <div className="home">
+        <div className="home nav-item">
           <Link to="/home">
-            <HomeOutlinedIcon />
+            <span className="icon">
+              <HomeOutlinedIcon />
+            </span>
             Home
           </Link>
         </div>
 
-        <div className="Search">
-          <SearchOutlinedIcon />
+        <div className="Search nav-item">
+          <span className="icon">
+            <SearchOutlinedIcon />
+          </span>
           Search
         </div>
 
-        <div className="messages">
+        <div className="messages nav-item">
           <Link to="/message">
-            <MailOutlineIcon />
+            <span className="icon">
+              <MailOutlineIcon />
+            </span>
             Messages
           </Link>
         </div>
 
-        <div className="notifications">
-          <NotificationsNoneIcon />
+        <div className="notifications nav-item">
+          <span className="icon">
+            <NotificationsNoneIcon />
+          </span>
           <button>Notifications</button>
         </div>
+        
       </div>
     </div>
   )
